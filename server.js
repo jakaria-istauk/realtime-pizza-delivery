72ca6096b60546a42ce729a36759da08d9dce32d
jakariaistauk <jakariamd35@gmail.com>
2020-12-22 23:12:16 +0600

    separate routes file

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const ejs = require('ejs')
 const expressLayout = require('express-ejs-layouts')
 const path = require('path')
+const routes = require('./routes/web')
 
 const PORT = process.env.PORT || 3000
 
@@ -15,22 +16,8 @@ app.use(expressLayout);
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
-
-app.get('/', (req, res) => {
-    res.render('home') 
-})
-
-app.get('/cart', (req, res)=>{
-    res.render('customers/cart')
-})
-
-app.get('/login', (req, res)=>{
-    res.render('auth/login')
-})
-
-app.get('/register', (req, res)=>{
-    res.render('auth/register')
-})
+//require routes
+routes(app)
 
 app.listen(PORT, () => {
     console.log('listenig on port '+PORT);

ac842c861e5218437ce6b7f51644925ad855fde9
jakariaistauk <jakariamd35@gmail.com>
2020-12-23 23:22:46 +0600

    session settup

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const ejs = require('ejs')
@@ -5,9 +6,12 @@ const expressLayout = require('express-ejs-layouts')
 const path = require('path')
 const routes = require('./routes/web')
 const mongoose = require('mongoose')
-
+const session = require('express-session')
+const flash = require('express-flash')
+const MongoDbStore = require('connect-mongo')(session)
+ 
 //database connection
-const db_url = 'mongodb://localhost/pizza';
+const db_url = process.env.MONGO_DB_URL;
 mongoose.connect(db_url, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true });
 const connection = mongoose.connection;
 connection.once('open', ()=>{
@@ -15,6 +19,24 @@ connection.once('open', ()=>{
 }).catch(err=>{
     console.log('Connection Faild......');
 })
+
+//session store
+let mongoStore = new MongoDbStore({
+    mongooseConnection: connection,
+    collection: 'sessions'
+})
+
+//Sessions config
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    store: mongoStore,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 } //24 hours
+}))
+
+app.use(flash())
+
 
 const PORT = process.env.PORT || 3000
 
